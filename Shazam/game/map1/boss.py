f347b075a6d20d5ce83d381cbe5b8695bfd41b74
Damian Muszyński <muszynskidamian@wp.pl>
2016-05-24 19:35:56 +0200

    Create boss.py

---
+++ Shazam/game/map1/boss.py
@@ -0,0 +1,17 @@
+from sys import exit
+import random
+import time
+
+from ..core import Fight
+from ..chars1 import SpiderLv1
+
+class AryanBunker:
+	
+	def start(self):
+		print "I am Aryan."
+		print "Fight!"
+		spider = SpiderLv1()
+		fight1 = Fight(spider, 1)
+		fight1.fight()
+		time.sleep(2)
+		return 0
