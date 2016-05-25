7215c59ddb290195dadb1abfa1a7b2db53a0cf50
Damian Muszyński <muszynskidamian@wp.pl>
2016-05-26 00:36:49 +0200

    boss fix

--- Shazam/game/map1/boss.py
+++ Shazam/game/map1/boss.py
@@ -2,16 +2,20 @@ from sys import exit
 import random
 import time
 
-from ..core import Fight
-from ..chars1 import SpiderLv1
+class Boss(object):
 
-class AryanBunker:
-	
 	def start(self):
-		print "I am Aryan."
-		print "Fight!"
-		spider = SpiderLv1()
-		fight1 = Fight(spider, 1)
-		fight1.fight()
-		time.sleep(2)
-		return 0
+		print "I am Boss, fight me!"
+		time.sleep(0.5)
+		print "What do you do?"
+		print "1. Fight."
+		what_do = raw_input("> ")
+		if what_do == "1":
+			print "You won, hurray!"
+			a = raw_input("Press any key to exit:\n>")
+			exit(0)
+			
+		else:
+			print "You can't do it, again."
+			foo = Boss()
+			foo.start()
