b5b6d5ddc0e1ded8994b71d2d1b0cededda58a6d
Damian Muszyński <muszynskidamian@wp.pl>
2016-06-02 21:12:18 +0200

    Update __init__.py

--- Shazam/game/core/__init__.py
+++ Shazam/game/core/__init__.py
@@ -3,3 +3,5 @@ from fight import Fight
 from player import Player
 from new_player import my_player
 from error_log import wrong_choice
+from int_input import int_input
+from choose_skill import choose_skill
